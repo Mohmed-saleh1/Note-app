0d145fc0a371939536a4b108043a2dc4bdc8f0ba
MU_SALEH <MU@git.com>
2023-05-08 23:37:52 +0300

    setup the routing for save , update and delete operations

--- route/noteRouter.js
+++ route/noteRouter.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const noteController = require("../controller/noteController")
 
-router.get('/notes',noteController.getAllNotes) 
+router.get('/notes',noteController.getAllNotes) ;
+router.post('/notes/save',noteController.saveNote) ;
+router.post('/update',noteController.updateNote) ;
+router.get('/delete',noteController.deleteNote) ;
 
 module.exports = router ;
