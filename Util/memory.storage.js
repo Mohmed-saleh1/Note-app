4081b144d35833f9a9508ea78e461083e3eac78d
MU_SALEH <MU@git.com>
2023-05-18 22:36:21 +0300

    setting id generation and memoryStorage

---
+++ Util/memory.storage.js
@@ -0,0 +1,13 @@
+var MemoryStorage = require('memorystorage');
+// here, the MemoryStorage function is available
+var memStorage = new MemoryStorage('my-app');
+
+
+
+
+
+
+
+
+
+exports.memStorage = memStorage;
